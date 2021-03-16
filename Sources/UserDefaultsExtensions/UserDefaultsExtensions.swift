529cc898544341de1dc49d001dee26c0f1520790
Sam Deane <sam@elegantchaos.com>
2021-03-16 12:10:22 +0000

    First version

--- Sources/UserDefaultsExtensions/UserDefaultsExtensions.swift
+++ Sources/UserDefaultsExtensions/UserDefaultsExtensions.swift
@@ -7,27 +7,29 @@ import Foundation
 
 
 extension UserDefaults {
+    
+    /// Write a value to the defaults if it has changed.
+    /// We perform a comparison first to avoid triggering unnecessary
+    /// notifications if the value is unchanged.
     func write(_ value: String, forKey key: String) {
         if string(forKey: key) != value {
             set(value, forKey: key)
         }
     }
     
+    /// Write a value to the defaults if it has changed.
+    /// We perform a comparison first to avoid triggering unnecessary
+    /// notifications if the value is unchanged.
     func write(_ value: Int, forKey key: String) {
         if integer(forKey: key) != value {
             set(value, forKey: key)
         }
     }
     
-    func read<T,V>(key: String, into path: ReferenceWritableKeyPath<T,V>, of: inout T) where V: RawRepresentable, V.RawValue: Equatable {
-        if let raw = value(forKey: key) as? V.RawValue, let value = V(rawValue: raw) {
-            if value != of[keyPath: path] {
-                of[keyPath: path] = value
-            }
-        }
-    }
-
-    func update<V>(_ value: inout V, fromKey key: String, default defaultValue: V? = nil) where V: UserDefaultsRepresentable, V: Equatable {
+    /// Read a value into a variable if it has changed.
+    /// We perform a comparison first to avoid triggering unnecessary
+    /// notifications (eg for ObservedObject properties) if the value is unchanged.
+    func read<V>(_ value: inout V, fromKey key: String, default defaultValue: V? = nil) where V: UserDefaultsRepresentable, V: Equatable {
         if let updated = (self.value(forKey: key) as? V) ?? defaultValue {
             if value != updated {
                 value = updated
@@ -35,7 +37,10 @@ extension UserDefaults {
         }
     }
 
-    func update<V>(_ value: inout V, fromKey key: String, default: V? = nil) where V: RawRepresentable, V.RawValue: Equatable {
+    /// Read a RawRepresentable value into a variable if it has changed.
+    /// We perform a comparison first to avoid triggering unnecessary
+    /// notifications (eg for ObservedObject properties) if the value is unchanged.
+    func read<V>(_ value: inout V, fromKey key: String, default: V? = nil) where V: RawRepresentable, V.RawValue: Equatable, V.RawValue: UserDefaultsRepresentable {
         if let raw = self.value(forKey: key) as? V.RawValue, let updated = V(rawValue: raw) {
             if value != updated {
                 value = updated
